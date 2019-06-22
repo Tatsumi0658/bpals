2c2368cf1bce553aa718931f1094e681935cd74a
koda <hogehogehoge@gmail.com>
2019-06-22 12:55:27 +0900

    画像の表示、helperによるviewの実装

---
+++ app/helpers/profile_helper.rb
@@ -0,0 +1,29 @@
+module ProfileHelper
+  def judge_age(age_value)
+    if age_value == 1
+      "10代後半~20代前半"
+    elsif age_value == 2
+      "20代後半"
+    elsif age_value == 3
+      "30代前半"
+    else
+      "30代後半以上"
+    end
+  end
+
+  def judge_skintype(skintype_value)
+    if skintype_value == 1
+      "混合肌"
+    else
+      "乾燥肌"
+    end
+  end
+
+  def judge_purpose(purpose_value)
+    if purpose_value == 1
+      "仕事"
+    else
+      "ファッション"
+    end
+  end
+end
