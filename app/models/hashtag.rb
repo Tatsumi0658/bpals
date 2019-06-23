ba92de21d076774077ca61ba213ad7ef8bc9ed61
koda <hogehogehoge@gmail.com>
2019-06-23 12:51:26 +0900

    hashtag,postsにrelatiohshipの記載追加

--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -1,2 +1,3 @@
 class Hashtag < ApplicationRecord
+  has_many :post_hashtag_relationships, through: :post_hashtag_relationships, source: :posts
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,14 @@
 class Post < ApplicationRecord
   validates :images, presence: true
+  has_many :post_hashtag_relationships, through: :post_hashtag_relationships, source: :hashtags
   mount_uploaders :images, ImageUploader
+
+  after_create do
+    post = Post.find_by(id: self.id)
+    hashtags = self.content.scan(/#[\w\p{Han}ぁ-ヶｦ-ﾟー]+/)
+    hashtags.each do |hash|
+      hash = hash.delete('#')
+      hash_log = Hashtag.find_or_create_by(hashtag: hash)
+      PostHashtagRelationship.create!(post_id: post.id, hashtag_id: hash_log.id)
+  end
 end
