4c94dac5de7d5b9ba9203a05faee45826e3e5ca8
koda <hogehogehoge@gmail.com>
2019-06-24 01:18:25 +0900

    いいね機能のAjax試行錯誤失敗

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,11 +1,11 @@
 class FavoritesController < ApplicationController
   def create
     favorite = current_profile.favorites.create(post_id: params[:post_id])
-    redirect_to post_path(post_id)
+    #redirect_to posts_url
   end
 
   def destroy
     favorite = current_profile.favorites.find_by(id: params[:id]).destroy
-    redirect_to post_path(post_id)
+    #redirect_to posts_url
   end
 end
