fba15d2382762edddbf0d24dd1e19599e816f3e5
koda <hogehogehoge@gmail.com>
2019-07-01 23:36:10 +0900

    omniauth_loginpath追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  root to:"profiles#index"
+  #get 'oauth/index'
+  root to:'oauth#index'
   devise_for :users, controllers: {
     registrations: 'users/registrations',
-    sessions: 'users/sessions'
+    sessions: 'users/sessions',
+    omniauth_callbacks: 'users/omniauth_callbacks'
   }
   devise_scope :user do
     get "user/:id", to: "users/registrations#detail"
