991c3819b641607015b4d9e633bbb25f477ededa
koda <hogehogehoge@gmail.com>
2019-07-06 12:47:39 +0900

    facebookログイン実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,4 +25,19 @@ class User < ApplicationRecord
     user.save
     user
   end
+
+  def self.find_for_facebook(auth)
+    user = User.find_by(email: auth.info.email)
+
+    unless user
+      user = User.new(
+        email:auth.info.email,
+        provider: auth.provider,
+        uid: auth.uid,
+        password: Devise.friendly_token[0,20],
+      )
+    end
+    user.save
+    user
+  end
 end
