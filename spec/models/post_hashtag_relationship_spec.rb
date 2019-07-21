ef18aac39b4d25a0bb029e5022479d51e8e20114
koda <hogehogehoge@gmail.com>
2019-07-21 18:13:57 +0900

    ハッシュタグと投稿の紐付けのmodelテスト記載

--- spec/models/post_hashtag_relationship_spec.rb
+++ spec/models/post_hashtag_relationship_spec.rb
@@ -2,4 +2,24 @@ require 'rails_helper'
 
 RSpec.describe PostHashtagRelationship, type: :model do
   #pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.create(:user)
+    @profile = Profile.create(
+      nickname: "foo",
+      image: "sample.jpg",
+      user_id: @user.id
+    )
+    @post = Post.create(
+      images: ["sample.jpg"],
+      content:"#sss",
+      profile_id: @profile.id
+    )
+    hashtag = @post.content.delete("#")
+    @hashtag = Hashtag.find_by(hashtag: hashtag)
+  end
+
+  it "ハッシュタグと投稿の紐付け" do
+    @relationship = PostHashtagRelationship.find_by(post_id: @post.id)
+    expect(@relationship.hashtag_id).to eq @hashtag.id
+  end
 end
