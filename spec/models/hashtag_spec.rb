cad8aa702d3d6ddfee90841944b04473cd4605d8
koda <hogehogehoge@gmail.com>
2019-07-21 18:11:11 +0900

    ハッシュタグのmodelテスト作成

--- spec/models/hashtag_spec.rb
+++ spec/models/hashtag_spec.rb
@@ -2,4 +2,23 @@ require 'rails_helper'
 
 RSpec.describe Hashtag, type: :model do
   #pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.create(:user)
+    @profile = Profile.create(
+      nickname: "foo",
+      image: "sample.jpg",
+      user_id: @user.id
+    )
+  end
+
+  it "ハッシュタグの作成" do
+    @post = Post.create(
+      images: ["sample.jpg"],
+      content:"#sss",
+      profile_id: @profile.id
+    )
+    hashtag = @post.content.delete("#")
+    @hashtag = Hashtag.find_by(hashtag: hashtag)
+    expect(@hashtag.hashtag).to eq hashtag
+  end
 end
