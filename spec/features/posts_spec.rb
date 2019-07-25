bacf324b924829f2dd30f8ad70dbd832246a8c7a
koda <hogehogehoge@gmail.com>
2019-07-26 01:48:11 +0900

    product投稿のfeature test実装

--- spec/features/posts_spec.rb
+++ spec/features/posts_spec.rb
@@ -31,7 +31,8 @@ RSpec.feature "Posts", type: :feature do
   before do
     @user = User.create(
       email: "foobar@example.com",
-      password: "123456"
+      password: "123456",
+      admin_flag: true
     )
     @profile = Profile.create(
       nickname: "foobar",
@@ -50,5 +51,57 @@ RSpec.feature "Posts", type: :feature do
     attach_file "post[images][]", "#{Rails.root}/spec/features/file/kkk.jpg", match: :first
     click_on "post"
     expect(page).to have_current_path(posts_path)
+  end
+
+  before do
+    @post = Post.create(
+      images: [Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/features/file/kkk.jpg'))],
+      profile_id: @profile.id
+    )
+  end
+
+  #scenario "Postのコメント投稿", js: true do
+  #  visit new_user_session_path
+  #  fill_in "user_email", with: "foobar@example.com"
+  #  fill_in "user_password", with: "123456"
+  #  click_on "Log in"
+
+  #  find('ul.post-list > li.post-item:nth-child(1) > a').click
+
+  #  fill_in "post_comment[content]", with: "サンプル"
+  #  click_on "post"
+
+  #  sleep 5
+
+    #save_and_open_page
+  #  expect(page).to have_content "サンプル"
+    #response.code.should == “200”
+  #end
+
+  scenario "Productの投稿" do
+    visit new_user_session_path
+    fill_in "user_email", with: "foobar@example.com"
+    fill_in "user_password", with: "123456"
+    click_on "Log in"
+
+    visit new_product_path
+    fill_in "product[name]", with: "sample_product"
+    attach_file "product[product_images][]", "#{Rails.root}/spec/features/file/kkk.jpg", match: :first
+    fill_in "product[description]", with: "sample"
+
+    click_on "post"
+
+    expect(page).to have_current_path(products_path)
+  end
+
+  scenario "Logout" do
+    visit new_user_session_path
+    fill_in "user_email", with: "foobar@example.com"
+    fill_in "user_password", with: "123456"
+    click_on "Log in"
+
+    click_on "Logout"
+
+    expect(page).to have_current_path(root_path)
   end
 end
