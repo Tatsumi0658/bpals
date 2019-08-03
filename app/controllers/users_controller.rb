5ad7ed40e28f9664ade5608695255f95fc7dd183
koda <hogehogehoge@gmail.com>
2019-08-03 23:34:30 +0900

    user/indexのview編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @users = User.where.not(id:current_user.id).all
+    @users = User.where.not(id:current_user.id).order("id")
   end
 
   def edit
