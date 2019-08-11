7c70dd76532270c38ff247d139c52e6de7c96be4
koda <hogehogehoge@gmail.com>
2019-08-11 17:58:29 +0900

    keysの変更

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,2 @@
 server '3.113.229.20', user: 'app', roles: %w{app db web}
-set :ssh_options, { forward_agent: true, user: fetch(:user), keys: '~/.ssh/id_rsa' }
+set :ssh_options, keys: '/Users/t_kodama/.ssh/id_rsa'
