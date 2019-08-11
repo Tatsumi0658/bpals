b3249bbe20a5adc7ea1bfb375940e059f5065d13
koda <hogehogehoge@gmail.com>
2019-08-11 17:06:57 +0900

    詳細追加

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,2 @@
 server '3.113.229.20', user: 'app', roles: %w{app db web}
-set :ssh_options, { port: 80, forward_agent: true, user: fetch(:user), keys: [File.expand_path('~/.ssh/id_rsa')], auth_methods: %w(publickey) }
+set :ssh_options, { forward_agent: true, user: fetch(:user), keys: [File.expand_path('~/.ssh/id_rsa')] }
