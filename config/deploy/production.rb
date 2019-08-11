59563be6dd6766424d450728003746e5db53f2a4
koda <hogehogehoge@gmail.com>
2019-08-11 16:46:02 +0900

    keyの所在変更

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,2 @@
 server '3.113.229.20', user: 'app', roles: %w{app db web}
-set :ssh_options, keys: [File.expand_path('/Users/t_kodama/.ssh/id_rsa.pem)')]
+set :ssh_options,{ keys: keys: '~/.ssh/bpals-key.pem'}
