a0cb5a40c36c4c5b46acb4428c8a398a8af5ba5f
koda <hogehogehoge@gmail.com>
2019-08-20 00:40:04 +0900

    sshのpath変更

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,2 @@
 server '3.113.229.20', user: 'app', roles: %w{app db web}
-ssh_options: { keys: %w(~/.ssh/id_rsa_bc:96:5e:a6:14:fc:80:91:4f:6f:02:e0:45:92:47:2c), forward_agent: true}
+set :ssh_options { keys: %w(~/.ssh/id_rsa), forward_agent: true, auth_methods: %w(publickey) }
