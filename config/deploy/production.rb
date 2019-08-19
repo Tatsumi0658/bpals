fe4f186e8534921d9d91e95419405dff91a9718d
koda <hogehogehoge@gmail.com>
2019-08-20 00:11:53 +0900

    keysの変更

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,2 @@
 server '3.113.229.20', user: 'app', roles: %w{app db web}
-set :ssh_options, keys: '/Users/t_kodama/.ssh/id_rsa'
+set :ssh_options, keys: '/Users/t_kodama/.ssh/id_rsa61:15:82:74:49:7f:72:da:ed:d4:b8:2b:d5:a6:41:77'
