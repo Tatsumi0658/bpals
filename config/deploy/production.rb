1066ef73fd4e288fe4948015e2950b362590d351
koda <hogehogehoge@gmail.com>
2019-08-21 23:45:25 +0900

    fingerprintの変更

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,2 @@
 server '3.113.229.20', user: 'app', roles: %w{app db web}
-set :ssh_options, { keys: %w(~/.ssh/id_rsa), forward_agent: true, auth_methods: %w(publickey) }
+set :ssh_options, keys: '~/.ssh/id_rsa'
