649f85cafe4d116f668d5cd5093e92ef444ea36e
koda <hogehogehoge@gmail.com>
2019-08-28 21:41:26 +0900

    投稿を降順に変更

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, only:[:new, :create, :edit, :update, :destroy]
   before_action :current_profile!, only:[:new, :create, :edit, :update, :destroy]
   def index
-    @posts = Post.page(params[:page]).per(12).includes(:profile)
+    @posts = Post.page(params[:page]).per(12).includes(:profile).order('updated_at DESC')
   end
 
   def new
