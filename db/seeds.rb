51e6cd83b33d1a88622c229996b0dd6f44b9c46f
koda <hogehogehoge@gmail.com>
2019-09-16 14:49:26 +0900

    rake db:seedの調整

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,12 @@
 #管理者情報
-#@user = User.create!(
-#            email: "admin@example.com",
-#            password: "111111",
-#            admin_flag: true
-#          )
-#Profile.create(
-#  nickname: "admin",
-#  user_id: @user.id
-#)
+@user = User.create!(
+            email: "admin@example.com",
+            password: "111111",
+            uid: "11111111-1111-1111-1111-111111111111",
+            provider: "",
+            admin_flag: true
+          )
+Profile.create(
+  nickname: "admin",
+  user_id: @user.id
+)
