1f40bcc1e7e73ebd1a241cd0518dccf77a8f5962
koda <hogehogehoge@gmail.com>
2019-09-16 14:42:55 +0900

    error確認

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
 #管理者情報
-@user = User.create!(
-            email: "admin@example.com",
-            password: "111111",
-            admin_flag: true
-          )
-Profile.create(
-  nickname: "admin",
-  user_id: @user.id
-)
+#@user = User.create!(
+#            email: "admin@example.com",
+#            password: "111111",
+#            admin_flag: true
+#          )
+#Profile.create(
+#  nickname: "admin",
+#  user_id: @user.id
+#)
